c12bc7ca8716774b3c433dafe6ef239ab5896eff
nood-the-gd <thanhtuongnguyen1@gmail.com>
2024-07-07 17:46:38 +0700

    making ui

---
+++ Assets/Scripts/CoreGame/CanvasUI/UI/ManagerUI/ManagerElementUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerElementUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _levelText;
+    [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private Image _icon;
+
+    public void SetData()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CoreGame/CanvasUI/UI/ManagerUI/ManagerTabUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ManagerTab
+{
+    ChiPhi,
+    TocDo,
+    NangSuat
+}
+
+public class ManagerTabUI : MonoBehaviour
+{
+    
+}
