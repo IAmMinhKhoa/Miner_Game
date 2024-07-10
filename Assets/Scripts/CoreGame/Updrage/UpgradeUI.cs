67c323cbb5b4b7c2d90070204a0b88c78fd71c61
Nguyen Vu <n.xvu96@gmail.com>
2024-07-10 20:09:34 +0700

    level detail ui

--- Assets/Scripts/CoreGame/Updrage/UpgradeUI.cs
+++ Assets/Scripts/CoreGame/Updrage/UpgradeUI.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NOOD;
+using UnityEngine.UI;
 
-public class UpgradeManager : MonoBehaviour
+public class UpgradeUI : MonoBehaviour
 {
-    [Header("Upgrade Panel Prefab")]
-    [SerializeField] private GameObject upgradePanelPrefab;
+    [Header("Upgrade Panel Elements")]
     [SerializeField] private GameObject m_upgradePanel;
+    [SerializeField] private Button closeButton;
 
     private Shaft _shaft;
     private ShaftUpgrade _shaftUpgrade;
@@ -14,17 +16,19 @@ public class UpgradeManager : MonoBehaviour
     #region ----Unity Methods----
     private void Start()
     {
-        m_upgradePanel = Instantiate(upgradePanelPrefab, GameUI.Instance.transform);
+        m_upgradePanel.transform.SetParent(GameUI.Instance.transform, false);
         m_upgradePanel.SetActive(false);
     }
     private void OnEnable()
     {
         ShaftUI.OnUpdrageRequest += ShowUpgradePanel;
+        closeButton.onClick.AddListener(ClosePanel);
     }
 
     private void OnDisable()
     {
         ShaftUI.OnUpdrageRequest -= ShowUpgradePanel;
+        closeButton.onClick.RemoveListener(ClosePanel);
     }
     #endregion
 
@@ -41,7 +45,17 @@ public class UpgradeManager : MonoBehaviour
                 break;
             }
         }
-        m_upgradePanel.SetActive(true);
+        ControlPannel(true);
+    }
+
+    private void ControlPannel(bool open)
+    {
+        m_upgradePanel.SetActive(open);
+    }
+
+    void ClosePanel()
+    {
+        ControlPannel(false);
     }
     
     #endregion
