4243298500529e3ae76d511bf296f10fda50129c
Nood-the-GD <thanhtuongnguyen1@gmail.com>
2024-07-25 17:39:10 +0700

    fix counter text is too high

--- Assets/Scripts/CoreGame/Updrage/CouterUpdrage.cs
+++ Assets/Scripts/CoreGame/Updrage/CouterUpdrage.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class CounterUpgrade : BaseUpgrade
 {
-    private Counter couter;
+    private Counter counter;
 
     protected override float CostsBoost
     {
-        get => couter.CostsBoost;
+        get => counter.CostsBoost;
     }
     private void Awake()
     {
-        couter = GetComponent<Counter>();
+        counter = GetComponent<Counter>();
     }
     protected override void RunUpgrade()
     {
         float nextScale = GetNextExtractionSpeedScale(CurrentLevel);
-        couter.BoostScale *= 1 + nextScale;
+        counter.BoostScale *= 1 + nextScale;
 
         if (IsNeedCreateTransporter(CurrentLevel))
         {
-            couter.CreateTransporter();
+            counter.CreateTransporter();
         }
     }
 
