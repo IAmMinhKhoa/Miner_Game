a375414d423cada2217643d7408db73575ef5d29
Tuong🫡 <admin@MacBook-Pro-cua-admin.local>
2024-09-16 21:09:07 +0700

    update(bank-ui): update bankUI sprite

--- Assets/Scripts/CoreGame/CanvasUI/UI/Bank/BankUI.cs
+++ Assets/Scripts/CoreGame/CanvasUI/UI/Bank/BankUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using NOOD.SerializableDictionary;
 using UnityEngine;
@@ -6,42 +7,17 @@ using UnityEngine.UI;
 
 public class BankUI : MonoBehaviour
 {
-    [SerializeField] private SerializableDictionary<int, int> _moneyPackageDictionary = new SerializableDictionary<int, int>();
-    [SerializeField] private SerializableDictionary<int, int> _pawPackageDictionary = new SerializableDictionary<int, int>();
-
-    [SerializeField] private MoneyButton _moneyBtnPref;
-    [SerializeField] private PawButton _pawBtnPref;
-    [SerializeField] private Transform _moneyContentTrans, _pawContentTrans;
     [SerializeField] private RectTransform _content;
     [SerializeField] private CanvasGroup _canvasGroup;
-    [SerializeField] private Button _closeButton;
+    [SerializeField] private List<Button> _closeButtons;
     [SerializeField] private float _fadeSpeed = 0.3f;
     private bool _isShowRequest;
 
     void Start()
     {
-        LoadFromDictionary();
-        _moneyBtnPref.gameObject.SetActive(false);
-        _pawBtnPref.gameObject.SetActive(false);
-        _closeButton.onClick.AddListener(Hide);
+        _closeButtons.ForEach(x => x.onClick.AddListener(Hide));
         if (_isShowRequest == false) // Prevent Hide when press shop button
             Hide();
-    }
-    private void LoadFromDictionary()
-    {
-        foreach (var moneyPair in _moneyPackageDictionary.Dictionary)
-        {
-            MoneyButton moneyBtn = Instantiate<MoneyButton>(_moneyBtnPref, _moneyContentTrans);
-            moneyBtn.gameObject.SetActive(true);
-            moneyBtn.SetData(moneyPair.Key, moneyPair.Value, this);
-        }
-        foreach (var pawPair in _pawPackageDictionary.Dictionary)
-        {
-            PawButton pawBtn = Instantiate<PawButton>(_pawBtnPref, _pawContentTrans);
-            pawBtn.gameObject.SetActive(true);
-            pawBtn.SetData(pawPair.Key, pawPair.Value, this);
-            LayoutRebuilder.ForceRebuildLayoutImmediate(_content);
-        }
     }
 
     // !Only call this function if transaction success
