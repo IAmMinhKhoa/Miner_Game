71946e756a99c68b20a56f41dc6fcfa6f3f4cb60
duong0011 <duongtruong0011@gmail.com>
2024-11-15 22:03:57 +0700

    Up

--- Assets/Scripts/CoreGame/CustomAtribute/ReadOnlyAttribute.cs
+++ Assets/Scripts/CoreGame/CustomAtribute/ReadOnlyAttribute.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEditor;
 
+// ReadOnly Attribute
 public class ReadOnlyAttribute : PropertyAttribute { }
 
+// Drawer cho ReadOnly Attribute
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 public class ReadOnlyDrawer : PropertyDrawer
 {
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
-		GUI.enabled = false; 
-		EditorGUI.PropertyField(position, property, label);
-		GUI.enabled = true; 
+		bool previousGUIState = GUI.enabled;
+		GUI.enabled = false;
+		EditorGUI.PropertyField(position, property, label, true);
+		GUI.enabled = previousGUIState;
 	}
 }
--- Assets/Scripts/CoreGame/Data/BannerSO.cs
+++ Assets/Scripts/CoreGame/Data/BannerSO.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 [CreateAssetMenu(fileName = "Banner Data", menuName = "ScriptableObjects/Baner")]
 public class BannerSO : ScriptableObject
 {
 	public List<Color> dataColorText = new List<Color>();
 	public List<TMP_FontAsset> dataFontText = new List<TMP_FontAsset>();
-	[ReadOnly]
 	public List<DesignTemplateInfo> dataSprite_1 = new();
 	public List<Sprite> dataSprite_2 = new List<Sprite>();
 
+	[SerializeField] Sprite _sprite;
 	//readonly string folderPath = "Assets/Sprites/Banner/BannerData/Mẫu Trang Trí";
 	private void OnValidate()
 	{
@@ -20,15 +21,35 @@ public class BannerSO : ScriptableObject
 	private void UpdateFileList(string folderPath, List<DesignTemplateInfo> dataSprite)
 	{
 		dataSprite.Clear();
-
+	
+		//SpriteLoader.LoadSpriteFromPath("Assets/Sprites/Banner/BannerData/Mẫu Trang Trí/Vintage Châu Âu/Đỏ - Tím.png");
 		if (Directory.Exists(folderPath))
 		{
-			string[] folder = Directory.GetDirectories(folderPath);
+			string[] folders = Directory.GetDirectories(folderPath);
 			int ID = 0;
-			foreach (string file in folder)
+
+			foreach (string folder in folders)
 			{
-				string fileName = Path.GetFileName(file);
+				string folderName = Path.GetFileName(folder);
+				List<string> sprites = new List<string>();
 				
+				foreach (string file in Directory.GetFiles(folder))
+				{
+					
+					if (Path.GetExtension(file) == ".png" || Path.GetExtension(file) == ".jpg")
+					{
+						string assetPath = file.Replace("\\", "/"); 
+						assetPath = assetPath.Replace(Application.dataPath, "Assets");
+
+						Debug.Log(assetPath);
+						sprites.Add(assetPath);
+					}
+				}
+				
+				if (sprites.Count > 0)
+				{
+					dataSprite.Add(new DesignTemplateInfo(ID++, sprites));
+				}
 			}
 		}
 		else
@@ -37,15 +58,25 @@ public class BannerSO : ScriptableObject
 		}
 	}
 }
+[System.Serializable]
 public struct DesignTemplateInfo
 {
+	[ReadOnly]
 	public int ID;
-	public List<Sprite> sprites;
+	[ReadOnly]
+	public List<string> sprites;
 
-	public DesignTemplateInfo(int ID, List<Sprite> sprites)
+	public DesignTemplateInfo(int ID, List<string> sprites)
 	{
 		this.ID = ID;
 		this.sprites = sprites;
+	}
+}
+public class SpriteLoader : MonoBehaviour
+{
+	public static Sprite LoadSpriteFromPath(string assetPath)
+	{
+		return AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
 	}
 }
 
