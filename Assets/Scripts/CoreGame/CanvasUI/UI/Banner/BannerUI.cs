213a5a2fa497bd73d40ffddb80b2ab6246a691f8
duong0011 <duongtruong0011@gmail.com>
2024-11-16 01:33:46 +0700

    update Banner

--- Assets/Scripts/CoreGame/CanvasUI/UI/Banner/BannerUI.cs
+++ Assets/Scripts/CoreGame/CanvasUI/UI/Banner/BannerUI.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class BannerUI : MonoBehaviour
 {
@@ -10,12 +11,17 @@ public class BannerUI : MonoBehaviour
 	[SerializeField] TMP_Text _demoTextTitle;
 	[SerializeField] Image _demobackBackground;
 	[SerializeField] Image _demofrontBackground;
+	[SerializeField] Image _demoFrontColor;
+	[SerializeField] Image _demoBackColor;
 
 	[Header("UI Input")]
 	[SerializeField] TMP_InputField inputTitle;
 	[SerializeField] TMP_Dropdown dropDownFont;
 	[SerializeField] TMP_Dropdown dropDownBrBack;
 	[SerializeField] TMP_Dropdown dropDownBrFront;
+	[SerializeField] TMP_Dropdown dropDownDetailBrFront;
+	[SerializeField] TMP_Dropdown dropDownDetailBrBack;
+
 
 	[Header("Prefab")]
 	[SerializeField] ColorButton colorBtnPrefab;
@@ -23,7 +29,9 @@ public class BannerUI : MonoBehaviour
 
 	BannerSO bannerData;
 	List<ColorButton> listColorButton = new();
-
+	
+	int curIndexFront = 0;
+	int curIndexBack = 0;
 	#region Unity Callbacks
 
 	private void Start()
@@ -36,8 +44,23 @@ public class BannerUI : MonoBehaviour
 			BannerEntity banner = JsonUtility.FromJson<BannerEntity>(json);
 			ApplyToWorldUIDemo(banner);
 			Debug.Log("Data loaded from PlayerPrefs.");
+			//return;
 		}
-
+		UpdateDemoImg();
+		//banner = new();
+	}
+	private void UpdateDemoImg()
+	{
+		var bn = BannerController.Instance.curBannerEntity;
+		Debug.Log(bn.IDFront + "-" + bn.indexBrFront + "-" + bn.IDBack + "-" + bn.indexBrBack);
+		_demobackBackground.sprite = bannerData.dataSprite_2[bn.IDBack].templateDetails[bn.indexBrBack].sprite;
+		_demofrontBackground.sprite = bannerData.dataSprite_1[bn.IDFront].templateDetails[bn.indexBrFront].sprite;
+		dropDownBrBack.value = bn.IDBack;
+		dropDownBrFront.value = bn.IDFront;
+		dropDownDetailBrBack.value = bn.indexBrBack;
+		dropDownDetailBrFront.value = bn.indexBrFront;
+		curIndexBack = bn.indexBrBack;
+		curIndexFront = bn.indexBrFront;
 	}
 	private void Init()
 	{
@@ -45,8 +68,10 @@ public class BannerUI : MonoBehaviour
 		string titleKeyTemplateNumber = LocalizationManager.GetLocalizedString(LanguageKeys.TitleBodyTemplateNumber);
 		bannerData = BannerController.Instance.bannerData;
 		PopulateDropdown(dropDownFont, bannerData.dataFontText.Count, titleKeyBodyFontCute + " ");
-		PopulateDropdown(dropDownBrBack, bannerData.dataSprite_1.Count, titleKeyTemplateNumber + " ");
-		PopulateDropdown(dropDownBrFront, bannerData.dataSprite_2.Count, titleKeyTemplateNumber + " ");
+		PopulateDropdown(dropDownBrBack, bannerData.dataSprite_2.Count, titleKeyTemplateNumber + " ");
+		PopulateDropdown(dropDownBrFront, bannerData.dataSprite_1.Count, titleKeyTemplateNumber + " ");
+		InitDetailDropDown(bannerData.dataSprite_2, 0, dropDownDetailBrBack, false);
+		InitDetailDropDown(bannerData.dataSprite_1, 0, dropDownDetailBrFront, true);
 	}	
 
 	private void OnEnable()
@@ -54,8 +79,11 @@ public class BannerUI : MonoBehaviour
 		Init();
 		inputTitle.onValueChanged.AddListener(SetDemoText);
 		dropDownFont.onValueChanged.AddListener(SetTextFont);
-		//dropDownBrBack.onValueChanged.AddListener(index => SetBackgroundImage(_demobackBackground, bannerData.dataSprite_1, index));
-		//dropDownBrFront.onValueChanged.AddListener(index => SetBackgroundImage(_demofrontBackground, bannerData.dataSprite_2, index));
+		dropDownBrBack.onValueChanged.AddListener(ID => InitDetailDropDown(bannerData.dataSprite_2, ID, dropDownDetailBrBack, false));
+		dropDownBrFront.onValueChanged.AddListener(ID => InitDetailDropDown(bannerData.dataSprite_1, ID, dropDownDetailBrFront, true));
+		dropDownDetailBrBack.onValueChanged.AddListener(index => SetBackgroundImage(_demobackBackground, bannerData.dataSprite_2, dropDownBrBack.value, index, false));
+		dropDownDetailBrFront.onValueChanged.AddListener(index => SetBackgroundImage(_demofrontBackground, bannerData.dataSprite_1, dropDownBrFront.value, index, true));
+		UpdateDemoImg();
 	}
 
 	private void OnDisable()
@@ -84,21 +112,50 @@ public class BannerUI : MonoBehaviour
 	{
 		_demoTextTitle.font = bannerData.dataFontText[index];
 	}
-
-	private void SetBackgroundImage(Image image, List<Sprite> sprites, int index)
+	void InitDetailDropDown(List<DesignTemplateInfo> templateInfos, int ID, TMP_Dropdown dropdownDetail, bool isFront)
 	{
-		image.sprite = sprites[index];
+		string titleKeyTemplateNumber = LocalizationManager.GetLocalizedString(LanguageKeys.TitleBodyTemplateNumber);
+		PopulateDropdown(dropdownDetail, templateInfos[ID].templateDetails.Count, titleKeyTemplateNumber + " ");
+
+		if (isFront)
+		{
+			_demoFrontColor.color = templateInfos[ID].templateDetails[0].color;
+			_demofrontBackground.sprite = templateInfos[ID].templateDetails[0].sprite;
+			curIndexFront = 0;
+		}
+		else
+		{
+			_demoBackColor.color = templateInfos[ID].templateDetails[0].color;
+			_demobackBackground.sprite = templateInfos[ID].templateDetails[0].sprite;
+			curIndexBack = 0;
+		}
+	}
+	private void SetBackgroundImage(Image image, List<DesignTemplateInfo> templateInfos,int ID ,int index, bool isFront)
+	{
+		image.sprite = templateInfos[ID].templateDetails[index].sprite;
+		if(isFront)
+		{
+			curIndexFront = index;
+			_demoFrontColor.color = templateInfos[ID].templateDetails[index].color;
+		}
+		else
+		{
+			curIndexBack = index;
+			_demoBackColor.color = templateInfos[ID].templateDetails[index].color;
+		}
 	}
 
 	public void SaveCurrentData()
 	{
 		// Collect the current data from the UI elements
-		var currentBanner = new BannerEntity(_demoTextTitle.text, _demoTextTitle.color, dropDownFont.value, dropDownBrBack.value, dropDownBrFront.value);
+		//BannerEntity(string title, Color color, int IDBack, int indexFont, int IDFront, int indexBrBack, int indexBrFront)
+		var currentBanner = new BannerEntity(_demoTextTitle.text, _demoTextTitle.color, dropDownFont.value, dropDownBrBack.value, dropDownBrFront.value, curIndexBack, curIndexFront);
 		string json = JsonUtility.ToJson(currentBanner);
 		PlayerPrefs.SetString("BannerData", json);
 		PlayerPrefs.Save();
-
+		BannerController.Instance.curBannerEntity = currentBanner;
 		// Apply to world UI
+		
 		BannerController.Instance.ApplyToWorldUI(currentBanner);
 		CloseModal();
 	}
@@ -109,8 +166,8 @@ public class BannerUI : MonoBehaviour
 		_demoTextTitle.text = banner.title;
 		_demoTextTitle.color = banner.color;
 		_demoTextTitle.font = bannerData.dataFontText[banner.indexFont];
-		//_demobackBackground.sprite = bannerData.dataSprite_1[banner.indexBrBack];
-		//_demofrontBackground.sprite = bannerData.dataSprite_2[banner.indexBrFront];
+		_demobackBackground.sprite = bannerData.dataSprite_1[banner.IDBack].templateDetails[banner.indexBrFront].sprite;
+		_demofrontBackground.sprite = bannerData.dataSprite_2[banner.IDBack].templateDetails[banner.indexBrBack].sprite;
 	}
 	#endregion
 
@@ -150,14 +207,18 @@ public class BannerEntity
 	public Color color;
 	public int indexFont;
 	public int indexBrBack;
+	public int IDBack;
 	public int indexBrFront;
+	public int IDFront;
 
-	public BannerEntity(string title, Color color, int indexFont, int indexBrBack, int indexBrFront)
+	public BannerEntity(string title, Color color, int indexFont, int IDBack, int IDFront, int indexBrBack, int indexBrFront)
 	{
 		this.title = title;
 		this.color = color;
 		this.indexFont = indexFont;
+		this.IDFront = IDFront;
 		this.indexBrBack = indexBrBack;
+		this.IDBack = IDBack;
 		this.indexBrFront = indexBrFront;
 	}
 }
--- Assets/Scripts/CoreGame/Data/BannerSO.cs
+++ Assets/Scripts/CoreGame/Data/BannerSO.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -10,67 +12,100 @@ public class BannerSO : ScriptableObject
 	public List<Color> dataColorText = new List<Color>();
 	public List<TMP_FontAsset> dataFontText = new List<TMP_FontAsset>();
 	public List<DesignTemplateInfo> dataSprite_1 = new();
-	public List<Sprite> dataSprite_2 = new List<Sprite>();
+	public List<DesignTemplateInfo>  dataSprite_2 = new();
 
-	[SerializeField] Sprite _sprite;
-	//readonly string folderPath = "Assets/Sprites/Banner/BannerData/Mẫu Trang Trí";
 	private void OnValidate()
 	{
-		UpdateFileList("Assets/Resources/Sprites/Banner/BannerData/Mẫu Trang Trí", dataSprite_1);
+		LoadSpirteData("Sprites/BannerData/Mẫu Trang Trí", dataSprite_1);
+		LoadSpirteData("Sprites/BannerData/Phông Nền", dataSprite_2);
 	}
-	private void UpdateFileList(string folderPath, List<DesignTemplateInfo> dataSprite)
+	void LoadSpirteData(string path, List<DesignTemplateInfo> list)
 	{
-		dataSprite.Clear();
-
-		_sprite = SpriteLoader.LoadSpriteFromPath("Assets/Sprites/Banner/BannerData/Mẫu Trang Trí/Vintage Châu Âu/Đỏ - Tím.png");
-		if (Directory.Exists(folderPath))
+		List<DesignTemplateInfo> tmpList = list.ToList();
+		list.Clear();
+		List<string> folderPaths = GetSubFolders(path);
+		int ID = 0;
+		foreach (var folderPath in folderPaths)
 		{
-			string[] folders = Directory.GetDirectories(folderPath);
-			int ID = 0;
+			List<Sprite> sprites = GetAllSpritesInFolder(folderPath);
+			List<TemplateDetail> templateDetails = new();
+			for(int i = 0; i < sprites.Count; i++) 
+			{
+				var template = sprites[i];
+				Color color = tmpList.Count > 0 ? tmpList[ID].templateDetails[i].color : Color.white;
+				templateDetails.Add(new TemplateDetail(color, template));
+			}
+			list.Add(new DesignTemplateInfo(ID++, templateDetails));
+		}
+	}
+	public static List<string> GetSubFolders(string parentFolderPath)
+	{
+		List<string> folderPaths = new List<string>();
+
+		string fullPath = Path.Combine("Assets/Resources", parentFolderPath);  
+
+		if (Directory.Exists(fullPath))
+		{
+			
+			string[] folders = Directory.GetDirectories(fullPath);
 
 			foreach (string folder in folders)
 			{
-				string folderName = Path.GetFileName(folder);
-				List<string> sprites = new List<string>();
 				
-				foreach (string file in Directory.GetFiles(folder))
-				{
-					
-					if (Path.GetExtension(file) == ".png" || Path.GetExtension(file) == ".jpg")
-					{
-						string assetPath = file.Replace("\\", "/"); 
-						assetPath = assetPath.Replace(Application.dataPath, "Assets");
-
-						Debug.Log(assetPath);
-						sprites.Add(assetPath);
-					}
-				}
-				
-				if (sprites.Count > 0)
-				{
-					dataSprite.Add(new DesignTemplateInfo(ID++, sprites));
-				}
+				folderPaths.Add(folder.Replace("\\", "/").Replace("Assets/Resources/", ""));
 			}
 		}
 		else
 		{
-			Debug.LogWarning($"Folder path '{folderPath}' does not exist.");
+			Debug.LogError($"Thư mục {parentFolderPath} không tồn tại!");
 		}
+
+		return folderPaths;
 	}
+	
+	public static List<Sprite> GetAllSpritesInFolder(string folderPath)
+	{
+		List<Sprite> sprites = new List<Sprite>();
+		Sprite[] loadedSprites = Resources.LoadAll<Sprite>(folderPath);
+		//Debug.Log(folderPath);
+		foreach (Sprite sprite in loadedSprites)
+		{
+			if (sprite != null)
+			{
+				sprites.Add(sprite);
+			}
+		}
+
+		return sprites;
+	}
+
 }
 [System.Serializable]
 public struct DesignTemplateInfo
 {
 	[ReadOnly]
 	public int ID;
-	[ReadOnly]
-	public List<string> sprites;
+	public List<TemplateDetail> templateDetails;
 
-	public DesignTemplateInfo(int ID, List<string> sprites)
+	public DesignTemplateInfo(int ID, List<TemplateDetail> templateDetails)
 	{
 		this.ID = ID;
-		this.sprites = sprites;
+		this.templateDetails = templateDetails;
 	}
+}
+[Serializable]
+public struct TemplateDetail
+{
+	public Color color;
+	[ReadOnly]
+	public Sprite sprite;
+
+	public TemplateDetail(Color color, Sprite sprite)
+	{
+		this.color = color;
+		this.sprite = sprite;
+	}
+
 }
 public class SpriteLoader : MonoBehaviour
 {
