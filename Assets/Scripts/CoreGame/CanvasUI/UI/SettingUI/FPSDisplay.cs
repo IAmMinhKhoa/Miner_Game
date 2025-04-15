d23bdea327bdc49907b63d2d21ef22ff6afe0952
hnghi <hnghi201003@gmail.com>
2025-04-15 14:30:06 +0700

    update fps

--- Assets/Scripts/CoreGame/CanvasUI/UI/SettingUI/FPSDisplay.cs
+++ Assets/Scripts/CoreGame/CanvasUI/UI/SettingUI/FPSDisplay.cs
@@ -8,7 +8,7 @@ public class FPSDisplay : MonoBehaviour
 
 	private int frameCount;
 	private float elapsedTime;
-	private const float updateRate = 0.5f; 
+	private const float updateRate = 0.5f;
 
 	private void Awake()
 	{
@@ -25,12 +25,7 @@ public class FPSDisplay : MonoBehaviour
 		if (elapsedTime >= updateRate)
 		{
 			float fps = frameCount / elapsedTime;
-			string color = fps switch
-			{
-				>= 60 => "#00FF00", 
-				>= 30 => "#C900FF", 
-				_ => "#FF0000"  
-			};
+			string color = "#0D0E0D"; 
 
 			fpsText.text = $"<color={color}><size=80%>FPS:</size> {fps:0}</color>";
 			frameCount = 0;
